0255349561b2aa93d23d7e88fc55c147b324de35
gardberg <lukasgardberg2@gmail.com>
2024-10-20 19:08:50 +0200

    use browser to render pdf

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,11 +1,12 @@
 export default function CV() {
   return (
     <div className="w-full h-screen">
-      <iframe
-        src="/Lukas Gardberg CV 2024.pdf"
+      <object
+        data="/Lukas Gardberg CV 2024.pdf"
+        type="application/pdf"
         className="w-full h-full"
-        title="Lukas Gardberg CV 2024"
-      />
+      >
+      </object>
     </div>
   );
 }
