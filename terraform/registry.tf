de34273ca0f76fb6667c6f31c8f1010935d363ab
gardberg <lukasgardberg2@gmail.com>
2025-01-02 16:28:49 +0100

    add permission to service ac

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -14,3 +14,11 @@ resource "google_artifact_registry_repository" "personal-repo" {
 #   role        = "roles/artifactregistry.reader"
 #   member      = "serviceAccount:${google_cloud_run_service.default.template[0].spec[0].service_account_name}"
 # }
+
+# Add permissions for CI/CD service account to push images
+resource "google_artifact_registry_repository_iam_member" "ci_admin" {
+  location    = google_artifact_registry_repository.personal-repo.location
+  repository  = google_artifact_registry_repository.personal-repo.name
+  role        = "roles/artifactregistry.admin"
+  member      = "serviceAccount:personal-dev-service-account@${var.project_id}.iam.gserviceaccount.com"
+}
